bdd200e94c89ed0cf258984cc9adf7cc165bb01a
Guillaume Depretz <gdepretz@gmail.com>
2019-02-15 03:17:26 +0100

    Update Bataille.java

--- src/Bataille/Bataille.java
+++ src/Bataille/Bataille.java
@@ -64,7 +64,57 @@ public class Bataille {
                 System.out.print("\n" + player2.getName() + " a gagné la main !");
             }
             else if(cardOfPlayeur1.toCompare(cardOfPlayeur2) == "eguale"){
-                System.out.print("\nil y a égalité. ");
+                System.out.print("\nil y a égalité.\n");
+                if(player1.getCard().size() >= 2 && player2.getCard().size() >= 2) {
+
+                    Card card1 = player1.getCard().get(0);
+                    Card card2 = player1.getCard().get(1);
+                    player1.takeACard(card1);
+                    player1.takeACard(card2);
+                    Card card3 = player2.getCard().get(0);
+                    Card card4 = player2.getCard().get(1);
+                    player2.takeACard(card3);
+                    player2.takeACard(card4);
+
+                    if(card2.toCompare(card4) == "win") {
+                        System.out.println(player1.getName() + " a tiré " + card2.getValue() + " de " + card2.getColor() + " et " + player2.getName() + " a tiré " + card4.getValue() + " de " + card4.getColor());
+                        System.out.print(player1.getName() + " a gagné la main !");
+                        player1.addACard(cardOfPlayeur1);
+                        player1.addACard(cardOfPlayeur2);
+                        player1.addACard(card1);
+                        player1.addACard(card2);
+                        player1.addACard(card3);
+                        player1.addACard(card4);
+                        player1.win();
+                    }
+
+                    else if(card2.toCompare(card4) == "loose") {
+                        System.out.println(player1.getName() + " a tiré " + card2.getValue() + " de " + card2.getColor() + " et " + player2.getName() + " a tiré " + card4.getValue() + " de " + card4.getColor());
+                        System.out.print(player2.getName() + " a gagné la main !");
+                        player2.addACard(cardOfPlayeur1);
+                        player2.addACard(cardOfPlayeur2);
+                        player2.addACard(card1);
+                        player2.addACard(card2);
+                        player2.addACard(card3);
+                        player2.addACard(card4);
+                        player2.win();
+                    }
+                    else if (card2.toCompare(card4) == "eguale"){
+                        System.out.println(player1.getName() + " a tiré " + card2.getValue() + " de " + card2.getColor() + " et " + player2.getName() + " a tiré " + card4.getValue() + " de " + card4.getColor());
+                        System.out.print("Il y a à nouveau égalité ! Personne n'a gagné.");
+                    }
+                }
+                else {
+
+                    if(player1.getCard().size() < 2) {
+                        player2.win();
+                        System.out.print(player1.getName() + " n'a plus de cartes. " + player2.getName() + " a gagné ! ");
+                    }
+                    else if(player2.getCard().size() < 2) {
+                        player1.win();
+                        System.out.print(player2.getName() + " n'a plus de cartes. " + player1.getName() + " a gagné ! ");
+                    }
+                }
             }
 
             // Récapitulatif de la main
